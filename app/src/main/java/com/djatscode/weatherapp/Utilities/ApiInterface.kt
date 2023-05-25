ea81153e17317b2185ff871e32a39f11e07a50aa
AryoDjat1 <aryodjati5@gmail.com>
2023-05-25 11:24:31 +0700

    add BASE URL and Other Utilities

--- app/src/main/java/com/djatscode/weatherapp/Utilities/ApiInterface.kt
+++ app/src/main/java/com/djatscode/weatherapp/Utilities/ApiInterface.kt
@@ -2,16 +2,19 @@ package com.djatscode.weatherapp.Utilities
 
 import com.djatscode.weatherapp.Models.WeatherModel
 import retrofit2.Call
+import retrofit2.http.GET
 import retrofit2.http.Query
 
 interface ApiInterface {
 
+    @GET("weather")
     fun getCurrentWeatherData(
         @Query("lat") lat: String,
         @Query("lon") lon: String,
         @Query("APPID") appid: String
     ): Call<WeatherModel>
 
+    @GET("weather")
     fun getCityWeatherData(
         @Query("q") q: String,
         @Query("APPID") appid: String
